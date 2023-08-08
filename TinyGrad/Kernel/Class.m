c905c58b9a3485ec76bca8459f3144821721770f
Nikolay Murzin <nikm@wolfram.com>
2023-08-08 16:46:37 +0300

    refactor Class inheritance

--- TinyGrad/Kernel/Class.m
+++ TinyGrad/Kernel/Class.m
@@ -4,7 +4,7 @@ PackageExport[Class]
 
 
 
-SubClass[cmd_String, class_, src_, self_, initArgs___] := With[{
+NewInstance[cmd : "New" | "Extend", class_, src_, self_, initArgs___] := With[{
     super = If[cmd === "New", self, self["Class"]]
 }, With[{
     ref = Replace[Unevaluated[src], {
@@ -12,43 +12,50 @@ SubClass[cmd_String, class_, src_, self_, initArgs___] := With[{
         Automatic | Except[_Symbol] :> Unique["$" <> StringDelete[ToString[Unevaluated[super]], Except[WordCharacter]]]
     }]
 },
-    ref /: DeleteObject[ref] := super["Free"[ref]];
+    If[ cmd === "Extend",
+        Language`ExtendedDefinition[ref] =
+            Language`ExtendedDefinition[self] /. HoldPattern[self] :> ref
+    ];
 
-    If[ FailureQ[Class[Unevaluated["Init"[ref, super]]]] ||
+
+    If[ (* every instance is also a class, initialize as class first *)
+        FailureQ[Class[If[cmd === "Extend", Unevaluated["Init"[ref, super, initArgs]], Unevaluated["Init"[ref, super]]]]] ||
+        (* initialize as instance *)
         cmd === "New" && FailureQ[class[Unevaluated["Init"[ref, initArgs]]]],
 
         DeleteObject[ref];
         Return[$Failed]
     ];
+
     If[ cmd === "New",
+        ref /: DeleteObject[ref] := super["Free"[ref]];
+
         ref["Label"] = Replace[Unevaluated[src], {
             Automatic :> ToString[Unevaluated[self]],
-            sym_Symbol :> SymbolName[sym],
+            sym_Symbol :> SymbolName[Unevaluated[sym]],
             _ :> ToString[Unevaluated[src]]
-        }],
+        }];
 
-        UpValues[ref] = UpValues[self] /. self -> ref;
+        (* can method be any expression? *)
+	    ref[method_String[args___]] := super[Unevaluated[method[ref, args]]];
+
+	    ref[prop_String] /; ListQ[super["Properties"]] && MemberQ[super["Properties"], prop] := super[prop[ref]];
+
+        (* Inheritance. Class instances only inherit methods. *)
+        If[ self === Class,
+            ref[classMethod : _String | _String[___]] := self[Unevaluated[classMethod]],
+            ref[args___] := Function[Null, self[##], HoldAll] @@ Unevaluated /@ Hold[args]
+        ];
+
+        MakeBoxes[ref, form_] ^:= super["Format"[ref, form]];
+
+        Normal[ref] ^:= ref["Normal"];
+
+        (* Sugar *)
+        Composition[ref, calls__] ^:= Fold[Function[Null, #1[Unevaluated[#2]], HoldRest], ref, Unevaluated[{calls}]];
+
+        SetAttributes[ref, {Temporary}];
     ];
-
-    (* can method be any expression? *)
-    ref[method_String[args___]] := super[Unevaluated[method[ref, args]]];
-
-    ref[prop_String] /; ListQ[super["Properties"]] && MemberQ[super["Properties"], prop] := super[prop[ref]];
-
-    MakeBoxes[ref, form_] ^:= super["Format"[ref, form]];
-
-    Normal[ref] ^:= ref["Normal"];
-
-    (* Inheritance *)
-    If[ self === Class,
-        ref[classMethod : _String | _String[___]] := self[Unevaluated[classMethod]],
-        ref[args___] := Function[Null, self[##], HoldAll] @@ Unevaluated /@ Hold[args]
-    ];
-
-    (* Sugar *)
-    Composition[ref, calls__] ^:= Fold[Function[Null, #1[Unevaluated[#2]], HoldRest], ref, Unevaluated[{calls}]];
-
-    SetAttributes[ref, {Temporary}];
 
     ref
 ]]
@@ -71,14 +78,16 @@ Class["Init"[self_, class_, initValues___]] := Block[{
     self["Methods"] = methods;
 
     self[(cmd : "New" | "Extend") | (cmd : "New" | "Extend")[initArgs___], src_ : Automatic] :=
-        SubClass[cmd, class, Unevaluated[src], self, initArgs];
+        NewInstance[cmd, class, Unevaluated[src], self, initArgs];
 
     self["Free"[obj_]] := (
         ClearAll[obj]
     );
-    self["Format"[obj_, form___]] := With[{boxes = ToBoxes[Tooltip[Framed[obj["Label"]], HoldForm[DisableFormatting[self]]], form]},
-        InterpretationBox[boxes, obj]
-    ];
+    If[ ! MatchQ[class, _Symbol] || ! MemberQ[Keys[DownValues[class]], Verbatim[HoldPattern][_["Format"[___]]]],
+	    self["Format"[obj_, form___]] := With[{boxes = ToBoxes[Tooltip[Framed[obj["Label"]], HoldForm[DisableFormatting[self]]], form]},
+	        InterpretationBox[boxes, obj]
+	    ];
+	];
 
     self
 ]
@@ -93,5 +102,5 @@ Class[class_, values___] := Class[Unevaluated["New"[class, values]]]
 Class[class : _Symbol | _String, values___] := Class[Unevaluated["New"[class, values]], Unevaluated[class]]
 
 Class[(class : _Symbol | _String)[parent_Symbol], values___] :=
-    HoldPattern[parent[class]] = parent[Unevaluated["New"[class, values]], Unevaluated[class]]
+    HoldPattern[parent[class]] = parent["Extend"[values], Unevaluated[class]]
 
